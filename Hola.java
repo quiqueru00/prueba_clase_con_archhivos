6e0266d78c7db1b46055ff091f1bb66f80f382ed
Enrique Ruiz <eruiz@iespabloserrano.com>
2025-09-25 08:58:11 +0200

    Añadir (ui): Saludo personalizado Pregunta el nombre al usuaruio y le saluda

--- Hola.java
+++ Hola.java
@@ -2,6 +2,16 @@ public class Hola {
 
     public static void main(String[] args) {
         
-        System.out.println("Hola");
+        Scanner sc = new Scanner(System.in);
+        String nombre;
+
+       
+            System.out.print("Introduce tu nombre: ");
+            nombre = sc.nextLine().trim(); // quitamos espacios en blanco al inicio y al final
+
+        // Saludo con el nombre introducido
+        System.out.println("¡Hola, " + nombre + "!");
+        
+        sc.close();
     }
 }
